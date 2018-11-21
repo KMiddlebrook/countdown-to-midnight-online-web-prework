46db81a18848b8839305e47f3e4fa306deec2157
Kaitlyn Middlebrook <kmiddlebrook2000@gmail.com>
2018-11-21 14:39:39 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,9 @@
-#write your code here
-
 def countdown(x)
-  countdown(x) == 10  
-  while countdown(x) > 0 
+  x = 10
+    while x > 0 
       puts "#{x} SECOND(S)!"
       x -= 1
-  end 
-  
-return "HAPPY NEW YEAR!"
+    end 
+  return "HAPPY NEW YEAR!"
 end
+
